0229ba868977b50f81ee82319236b6d1f74a7f7c
Sreerag Rajesh Babu <Sreerag.Babu@asl.mum.sst>
2024-07-23 20:09:04 +0530

    innit

---
+++ Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListObjectSerialization.Models
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"======================== \n" +
+                $"Id: {Id} \n" +
+                $"Name: {Name} \n" +
+                $"Email: {Email} \n";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text.Json;
+using ListObjectSerialization.Models;
+
+namespace ListObjectSerialization
+{
+    internal class Program
+    {
+        static string file = ConfigurationManager.AppSettings["filePath"]!.ToString();
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            persons.Add(new Person() { Id = 101, Name = "Ramesh", Email = "ramesh@gmail.com" });
+            persons.Add(new Person() { Id = 102, Name = "Suresh", Email = "suresh@gmail.com" });
+            persons.Add(new Person() { Id = 103, Name = "Mahesh", Email = "mahesh@gmail.com" });
+            persons.Add(new Person() { Id = 104, Name = "Ajesh",  Email = "ajesh@gmail.com" });
+            persons.Add(new Person() { Id = 105, Name = "Rakesh", Email = "rakesh@gmail.com" });
+
+          //  SerializeObject(persons);
+
+
+            DeserializeObject();
+            
+        }
+        static void SerializeObject(List <Person> persons)
+        {
+            int count = 0;
+            foreach (Person person in persons) { 
+                using(StreamWriter sw = new StreamWriter(file,true))
+                {
+                    sw.WriteLine(JsonSerializer.Serialize(person));
+                    count++;
+                    Console.WriteLine($"Object {count} has been serialized");
+                }
+            }
+        }
+
+        static void DeserializeObject()
+        {
+            List<Person> persons = new List<Person>();
+
+            using (StreamReader sr = new StreamReader(file))
+            {
+                string line;
+                while ((line = sr.ReadLine()!) != null)
+                {
+                    Person person = JsonSerializer.Deserialize<Person>(line)!;
+                    persons.Add(person);
+                }
+            }
+
+            Console.WriteLine("Deserialized Persons:");
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person);
+            }
+            Console.WriteLine(persons.Count);
+        }
+
+    }
+}
